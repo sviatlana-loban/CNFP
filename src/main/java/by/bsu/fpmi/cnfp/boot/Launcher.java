694fd3a64e39cffe65b312379c4638ab8ae83e60
Svetlana Kostyukovich <svetlana.kostyukovich@gmail.com>
2012-12-12 23:22:07 +0200

    initial commit

---
+++ src/main/java/by/bsu/fpmi/cnfp/boot/Launcher.java
@@ -0,0 +1,10 @@
+package by.bsu.fpmi.cnfp.boot;
+
+/**
+ * @author Svetlana Kostyukovich
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
